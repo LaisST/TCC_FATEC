7b1936a41374e3c90ed69f7d8f27a7740d71213e
Lais.ST <74365785+LaisST@users.noreply.github.com>
2023-09-12 10:57:57 -0300

    Add files via upload

---
+++ calendario/index.php
@@ -0,0 +1,22 @@
+<?php include("config/config.php"); ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/css/style.css'?>">
+
+    <title>Calendario Portal Educacional</title>
+</head>
+
+<body>
+<div class="calendar">
+</div>
+<script src="<?php echo DIRPAGE.'lib/js/fullcalendar/dist/index.global.min.js'; ?>"></script>
+<script src="<?php echo DIRPAGE.'lib/js/javascript.js'; ?>"></script>
+
+</body>
+</html>
