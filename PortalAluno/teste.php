6ac6c710acb5ab1f54336e8767f19ff0f17b6d25
Lais.ST <74365785+LaisST@users.noreply.github.com>
2023-10-15 17:47:34 -0300

    Add files via upload

---
+++ PortalAluno/teste.php
@@ -0,0 +1,8 @@
+<?php
+    // require_once 'src/utilitarios/EnvioEmail.php';
+
+    // $envioEmail = new EnvioEmail();
+
+    // $envioEmail->enviarEmail('laiscostast2@gmail.com', 'Teste', 'Hello World!');
+
+    echo ' Hello World!';
